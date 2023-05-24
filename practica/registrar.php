aea9639435139a5e716536213e57b2e080e676ba
Confley <confleyesparza@gmail.com>
2023-05-24 08:12:36 -0500

    Update registrar.php

--- practica/registrar.php
+++ practica/registrar.php
@@ -13,6 +13,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   // Obtener los datos del formulario
   $usuario = $_POST['Usuario'];
   $comentario = $_POST['Comentario'];
+  // $comentario = htmlspecialchars($_POST['Comentario'], ENT_QUOTES, 'UTF-8');
+  // $comentario = mysqli_real_escape_string($conexion, $_POST['Comentario']);
+
 
   // Preparar la consulta parametrizada
   $consulta_registrar = "INSERT INTO comentarios(Usuario, Comentario) VALUES ('$usuario', '$comentario')";
